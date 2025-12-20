3507c87bbb5824d21e2b4c286ee7bacba0abe0a1
Chensao <postmaster@chensao.im>
2025-12-19 21:54:24 -0500

    Submission for entities package. Version 2.0 (A lot of things needs to be changed) (Currently everything in Chinese because it is easier for me to read, I will change it before next Monday) Expected to have minor updates throughout the weekend But, now can be used to develop other systems.

--- src/main/java/entities/items/Item.java
+++ src/main/java/entities/items/Item.java
@@ -1,68 +1,68 @@
 package entities.items;
 
-import systems.gacha.interfaces.Displayable;
-import systems.gacha.interfaces.GachaPullable;
+import entities.abs.GameEntity;
+import entities.GachaPullable;
 
-public abstract class Item implements Displayable, GachaPullable {
-    protected String name;
-    protected int id;
-    protected String description;
-    protected int rarity; // 3, 4, or 5 stars
-    protected boolean isUpItem; // is up character or not
+public abstract class Item extends GameEntity implements GachaPullable {
+    protected int value;
+    protected int weight;
+    protected boolean stackable;
 
-    public Item(String name, int id, int rarity, boolean isUpItem) {
-        this.name = name;
-        this.id = id;
-        this.rarity = rarity;
-        this.isUpItem = isUpItem;
-        this.description = "";
+    public Item() {
+        super();
+        this.value = 10;
+        this.weight = 1;
+        this.stackable = true;
     }
 
-    // Getters
-    public String getName() {
-        return this.name;
+    public Item(String name, int value) {
+        super(name, nextId);
+        this.value = value;
+        this.weight = 1;
+        this.stackable = true;
     }
 
-    public int getId() {
-        return this.id;
+    public Item(String name, int id, int value, int weight, boolean stackable) {
+        super(name, id);
+        this.value = value;
+        this.weight = weight;
+        this.stackable = stackable;
     }
 
-    public int getRarity() {
-        return this.rarity;
-    }
-
-    public boolean isUpItem() {
-        return this.isUpItem;
-    }
-
-    // Displayable interface implementation
-    @Override
-    public void displayInfo() {
-        StringBuilder sb = new StringBuilder();
-        //  = "★".repeat(rarity)
-        String rarityStarStr = "";
-        for (int i = 0; i < getRarity(); i++) {
-            sb.append("★");
-        }
-        rarityStarStr = sb.toString();
-        String upStatus = isUpItem ? "[UP]" : "";
-        System.out.printf("%-15s %-10s Rarity: %s %s%n",
-                name, upStatus, rarityStarStr, description);
-    }
+    public abstract void use();
 
     @Override
-    public String toCSVFormat() {
-        return String.format("%s,%d,%d,%b", name, id, rarity, isUpItem);
-    }
-
-    // GachaPullable interface implementation
-    @Override
-    public Object pull() {
+    public Item pull() {
+        System.out.println("抽取物品: " + name);
         return this;
     }
 
     @Override
     public double getDropRate() {
-        return 0; // To be overridden by subclasses
+        // 基础掉落率，可以根据价值调整
+        return 1.0 / (value / 10.0);
     }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("=== 物品信息 ===");
+        System.out.println("名称: " + name);
+        System.out.println("价值: " + value);
+        System.out.println("重量: " + weight);
+        System.out.println("可堆叠: " + (stackable ? "是" : "否"));
+        System.out.println("描述: " + description);
+    }
+
+    @Override
+    public String toCSVFormat() {
+        return super.toCSVFormat() + String.format(",%d,%d,%b", value, weight, stackable);
+    }
+
+    // Getter 和 Setter
+    public int getValue() { return value; }
+    public void setValue(int value) { this.value = value; }
+    public int getWeight() { return weight; }
+    public void setWeight(int weight) { this.weight = weight; }
+    public boolean isStackable() { return stackable; }
+    public void setStackable(boolean stackable) { this.stackable = stackable; }
 }
